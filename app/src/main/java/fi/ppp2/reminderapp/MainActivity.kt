9465eae1b2643c6e414632d9edf20d92ef643d1d
ppp2 <pyykkonen.patrik@gmail.com>
2020-03-22 02:52:20 +0200

    HW2 Complete

--- app/src/main/java/fi/ppp2/reminderapp/MainActivity.kt
+++ app/src/main/java/fi/ppp2/reminderapp/MainActivity.kt
@@ -3,6 +3,7 @@ package fi.ppp2.reminderapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
